44ad2ef8127b2f9b95e890f802bc10d0492437a2
JohnGoldGG <61142623+JohnGoldGG@users.noreply.github.com>
2020-03-16 16:49:41 +0300

    Домашнее задание№2. Гришенко Ростислав 1-ПМИ-1

--- src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
@@ -12,10 +12,43 @@ public class MaxTask {
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      *
      */
+   public static int[] getMaxArray(int[] array, int count) {
+       int[] result = new int[count];
+       result[0] = 3;
+       result[1] = array[1];
+       result[2] = array[2];
+       Arrays.sort(array);
+
+       for (int i = 0; i < array.length; i++) {
+           result[i] = array[i];
+       }
+       return null;
+   }
+}
+
+
+    /*
+    public static int[] getMaxArray(int[] array, int count) {
+        for (int i = 0; i < array.length ; i++) {
+            if (array.length < count) {
+                return null;
+            } else {
+                int j = 100;
+               do{
+                   if (array[j] > array[i]) j = i;
+                   return array[i - j];
+               } while (array.length == count);
+               return
+            }
+        }
+    }
+}
+*/
+    /*
     public static int[] getMaxArray(int[] array, int val) {           // Поменял count на val
         int count = 0;
         // Нахождение длины массива
-        for (int i = 0; i < val; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] != val) {
                 count++;
             } else {
@@ -28,7 +61,7 @@ public class MaxTask {
         int[] newArray = new int[val];
         int offset = 0;
         // Запись в нвоый массив
-        for(int i = 0; i < val; i++) {
+        for(int i = 0; i < array.length; i++) {
             if (array[i] == count) {
                 offset++;
             } else {
@@ -38,3 +71,4 @@ public class MaxTask {
         return newArray;
     }
 }
+*/
